a2fbd0d18a550c5772f8a499cc15c2dd537e70c9
lisongyu <269876940@qq.com>
2020-10-27 06:50:52 +0800

    提交选项卡切换成功

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import data from './data'
+import Dl from './Dl'
+export default class App extends Component {
+  state ={
+    openName:''
+  }
+  changeOpen=(name)=>{
+    this.setState({openName:name})
+  }
+  render() {
+    let {openName}=this.state
+    return (
+      <div className="friend-list">
+        {/* 注意data是一个对象,而非数组*/}
+        {
+          Object.keys(data).map(itemName=>{
+            {/* key的作用和取值在文中详细介绍*/}
+            return <Dl 
+              key={itemName}
+              dlData={data[itemName]}
+              name = {itemName}
+              openName = {openName}
+              changeOpen = {this.changeOpen}
+            
+            />
+          })
+        }
+        {/* <Dl /> */}
+      </div>
+    )
+  }
 }
 
-export default App;
